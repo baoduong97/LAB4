8de9130087d22f587dc7732bd47a1ca8f83bcedf
Bao Duong <thaibao97@outlook.com>
2019-12-29 20:06:50 +0700

    Done Lab4.2 all

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-10.times do
+# require 'action_view'
+# require 'action_view/helpers'
+include ActionView::Helpers::DateHelper
+# include ActionController::
+# include ::TableFieldHelper
+# include ActionController::Base
+# include ActionController::Helpers::ClassMethods
+3.times do
     username = Faker::Name.name
     caption = Faker::Quote.famous_last_words 
     # url = Faker::LoremPixel.image("500x500", false, 'sports', nil, username)
     url  = Faker::LoremPixel.image(size: "500x600", is_gray: false, category: 'sports', number: nil, text: username)
     like = Faker::Number.number(digits: 4)
-    Photo.create!(url: url, username: username, caption: caption, likes_count: like)
+    time_ago = Faker::Number.number(digits: 1)
+    # from_time = (time_ago.year).ago
+    time_url = time_ago_in_words(time_ago.year.ago) 
+    Photo.create!(url: url, username: username, caption: caption, likes_count: like, avatar_url:time_url)
 end
